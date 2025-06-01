a8ed8b75fc3bc02473b5487867d3724228731685
Samir Mangroliya <sbmangroliya2012@gmail.com>
2025-06-01 11:31:40 +0530

    project setup in android studio in Run Condition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,7 +48,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.koin.androidx.compose)
-    implementation(libs.koin.android)
 }
