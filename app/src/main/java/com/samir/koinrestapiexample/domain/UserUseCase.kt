8102519cc62156ee6ae5743257814d3169109f48
Samir Mangroliya <sbmangroliya2012@gmail.com>
2025-06-03 18:41:04 +0530

    Rest API call added in Repository class

--- app/src/main/java/com/samir/koinrestapiexample/domain/UserUseCase.kt
+++ app/src/main/java/com/samir/koinrestapiexample/domain/UserUseCase.kt
@@ -1,14 +1,4 @@
 package com.samir.koinrestapiexample.domain
 
-import com.samir.koinrestapiexample.model.User
-import com.samir.koinrestapiexample.network.UserApiService
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
-
-class UserUseCase: KoinComponent {
-    val userApiService: UserApiService by inject()
-
-    suspend fun getUserList(): List<User> {
-        return userApiService.getUsers()
-    }
+class UserUseCase {
 }
--- app/src/main/java/com/samir/koinrestapiexample/repository/UserRepository.kt
+++ app/src/main/java/com/samir/koinrestapiexample/repository/UserRepository.kt
@@ -1,4 +1,14 @@
 package com.samir.koinrestapiexample.repository
 
-class UserRepository {
+import com.samir.koinrestapiexample.model.User
+import com.samir.koinrestapiexample.network.UserApiService
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class UserRepository : KoinComponent {
+    val userApiService: UserApiService by inject()
+
+    suspend fun getUserList(): List<User> {
+        return userApiService.getUsers()
+    }
 }
