355f02c51253376c1f9e11aefe49f5412fe57ee4
Samir Mangroliya <sbmangroliya2012@gmail.com>
2025-06-11 14:45:01 +0530

    Junit Testing added for filter user list

---
+++ app/src/test/java/com/samir/koinrestapiexample/FilterUserListTest.kt
@@ -0,0 +1,41 @@
+package com.samir.koinrestapiexample
+
+import com.samir.koinrestapiexample.data.model.User
+import com.samir.koinrestapiexample.domain.UserRepository
+import com.samir.koinrestapiexample.domain.UserUseCase
+import org.junit.Before
+import org.junit.Test
+
+
+class FilterUserListTest {
+    private lateinit var userUseCase: UserUseCase
+
+    @Before
+    fun setup(){
+        userUseCase = UserUseCase(FakeUserRepository())
+    }
+
+    @Test
+    fun checkFilterList() {
+        val list = (1..5).map {
+            User(
+                if(it%2!=0) "user$it@test.biz" else "nouser@gmail.com" ,
+                id = it,
+                name = "",
+                phone = "",
+                username = "",
+                website = ""
+            ) }.toList()
+
+        val result = userUseCase.getFilterDataByEmail("biz", list)
+        println("total:: ${result.size}")
+
+        assert(result.size == 3)
+    }
+
+    class FakeUserRepository : UserRepository {
+        override suspend fun getUserList(): List<User> {
+            return emptyList()
+        }
+    }
+}
